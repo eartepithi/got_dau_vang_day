42be342159c911cd9c6bd9c773ddfd517732d75d
eartepithi <115027444+eartepithi@users.noreply.github.com>
2022-11-05 16:42:26 +0000

    fix

---
+++ c-o-m-i-k-e-y.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         c-o-m-i-k-e-y
+// @namespace    https://github.com/eartepithi/got_dau_vang_day/raw/main/c-o-m-i-k-e-y.user.js
+// @version      2022.11.05
+// @description  down c-o-m-i-k-e-y
+// @author       You
+// @match        https://comikey.com/read/*/*/*/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=comikey.com
+// @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
+// @grant        none
+// @downloadURL  https://github.com/eartepithi/got_dau_vang_day/raw/main/c-o-m-i-k-e-y.user.js
+// @updateURL    https://github.com/eartepithi/got_dau_vang_day/raw/main/c-o-m-i-k-e-y.meta.js
+// ==/UserScript==
